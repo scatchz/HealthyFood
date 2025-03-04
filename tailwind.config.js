6592171fc8bfd29302417bf41074dcb6f4195009
unknown <nsaodat05@gmail.com>
2025-03-04 23:48:06 +0500

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

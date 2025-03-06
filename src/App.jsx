8bbfc10deb1b7da40164574016bc20e0f8cce3b7
unknown <nsaodat05@gmail.com>
2025-03-06 21:49:19 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+// import { Index } from "./pages/Index/Index";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Index />} /> */}
+          {/* <Route path="/About" element={<About />} /> */}
+        </Route>
+        <Route path="*" element={<div>404 Not found</div>} />
+        {/* <Route path="/Test" element={<Test />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;

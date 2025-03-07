51695c08638f35e6b9d0a558a9db3205fc221342
Saodat <nsaodat05@gmail.com>
2025-03-07 20:53:28 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
+// import { Main } from "./components/Main";
 // import { Index } from "./pages/Index/Index";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* <Route index element={<Index />} /> */}
-          {/* <Route path="/About" element={<About />} /> */}
+          {/* <Route path="/Main" element={<Main />} /> */}
         </Route>
         <Route path="*" element={<div>404 Not found</div>} />
         {/* <Route path="/Test" element={<Test />} /> */}

0b76a0a7d3c17c70c839253693c6adf12350e7fe
unknown <nsaodat05@gmail.com>
2025-03-08 14:08:40 +0500

    fix

---
+++ src/pages/Dostavka.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export const Dostavka = () => {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+};
---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Dostavka } from "./Dostavka.jsx";
+
+
+export const Main = () => {
+  return (
+    <div>
+      <Dostavka />
+    </div>
+  );
+};
